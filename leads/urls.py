bcdd66016f917c3d0a9ccc19e45a8a2b18ef0286
elijah obara <elijahobara357@gmail.com>
2023-04-01 11:55:52 +0300

    Hot fix

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lead_list, lead_detail,lead_create,lead_update
+from .views import lead_list, lead_detail,lead_create,lead_update,lead_delete
 
 app_name = "leads"
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', lead_list),
     path('<int:pk>/', lead_detail),
     path('<int:pk>/update/', lead_update),
+    path('<int:pk>/delete/', lead_delete),
     path('create/', lead_create)
 ]
  
--- leads/views.py
+++ leads/views.py
@@ -44,3 +44,9 @@ def lead_update(request, pk):
         "form": form
     }
     return render(request, "leads/lead_update.html", context) 
+
+def lead_delete(request, pk):
+    lead =Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+    
