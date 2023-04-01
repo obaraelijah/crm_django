dbcb1ab5f0fa972102895eac97ce1beb573daf1b
elijah obara <elijahobara357@gmail.com>
2023-04-01 11:43:09 +0300

    created a cretae and delete view

--- leads/views.py
+++ leads/views.py
@@ -31,3 +31,16 @@ def lead_create(request):
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+def lead_update(request, pk):
+    lead =Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context={
+        "form": form
+    }
+    return render(request, "leads/lead_update.html", context) 
