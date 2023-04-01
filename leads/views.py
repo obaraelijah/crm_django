1a786f510826ec3374db127486878f8f1b99fffc
elijah obara <elijahobara357@gmail.com>
2023-04-01 09:50:38 +0300

    Context

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 def home_page(request):
-    return render(request, "second_page.html")
+    leads = Lead.objects.all()
+    
+    context={
+        "leads": leads
+    }
+    return render(request, "second_page.html", context) 
