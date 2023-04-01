893a6ed5b150d171032f8bba6ef71408d1c90590
elijah obara <elijahobara357@gmail.com>
2023-04-01 11:31:26 +0300

    Hot fix in django form

--- leads/forms.py
+++ leads/forms.py
@@ -1,6 +1,12 @@
 from  django import forms 
+from.models import Lead
 
-class LeadForm(forms.Form):
-    first_name = forms.CharField()
-    last_name= forms.CharField()
-    age = forms.IntegerField(min_value=0)
+class LeadModelForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            'first_name',
+            'last_name',
+            'age',
+            'agent',
+        )
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import  LeadModelForm
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -19,21 +19,13 @@ def lead_detail(request, pk):
     return render(request, "leads/lead_detail.html", context) 
 
 
+
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name'] 
-            last_name = form.cleaned_data['last_name'] 
-            age = form.cleaned_data['age']
-            agent= Agent.objects.first()
-            Lead.objects.create(
-                first_name = first_name,
-                last_name = last_name,
-                age = age,
-                agent = agent
-            )
+            form.save()
             return redirect("/leads")
     context={
         "form": form
