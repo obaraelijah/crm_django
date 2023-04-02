205c62c23e91e5e3f1952deefd05af0918f59787
elijah obara <elijahobara357@gmail.com>
2023-04-02 15:13:36 +0300

    Password logic implime

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -3,7 +3,15 @@ from django.urls import path, include
 from leads.views import LandingPageView,SignUpView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView, 
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,7 +20,11 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace="agents")),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
 ]
 
