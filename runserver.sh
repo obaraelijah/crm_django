69e8b885131adfd0f53f2bd99f6f320d648a156f
elijah obara <elijahobara357@gmail.com>
2023-04-04 21:21:36 +0300

    run .sh file

--- runserver.sh
+++ runserver.sh
@@ -4,3 +4,11 @@ python manage.py migrate
 
 gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi
 
+
+
+username = doadmin
+password = AVNS_SzxQc6pTXGbcDCp154j
+host = db-postgresql-nyc1-29986-do-user-13887935-0.b.db.ondigitalocean.com
+port = 25060
+database = defaultdb
+sslmode = require
