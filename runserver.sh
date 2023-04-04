832819d218940deaebb5d4ef015f42022b47ac4c
elijah obara <elijahobara357@gmail.com>
2023-04-04 21:21:48 +0300

    run .sh file

--- runserver.sh
+++ runserver.sh
@@ -6,9 +6,3 @@ gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi
 
 
 
-username = doadmin
-password = AVNS_SzxQc6pTXGbcDCp154j
-host = db-postgresql-nyc1-29986-do-user-13887935-0.b.db.ondigitalocean.com
-port = 25060
-database = defaultdb
-sslmode = require
